5eb1e2364006701b423833696d1c5bdd0ea9cbd2
Jesper Bisgaard <jb@interflora.dk>
2019-12-08 21:47:52 +0100

    Updated decoder.

--- src/Service/Decoder.php
+++ src/Service/Decoder.php
@@ -14,49 +14,49 @@ use Interflora\CdpApi\Model\Business;
 class Decoder {
   public function decodeAccountResponse($response) {
     $account = new Account();
-    $account->setId($response->id)
-      ->setDebitorNumber($response->debitorNumber)
-      ->setType($response->type)
-      ->setEmail($response->email)
-      ->setLastName($response->lastName)
-      ->setFirstName($response->firstName)
-      ->setPwd1($response->pwd1)
-      ->setTelephone($response->telephone)
-      ->setGender($response->gender);
-    if ($response->address) {
-      $account->setAddress($this->decodeAddressResponse($response->address));
+    $account->setId($response['id'])
+      ->setDebitorNumber($response['debitorNumber'])
+      ->setType($response['type'])
+      ->setEmail($response['email'])
+      ->setLastName($response['lastName'])
+      ->setFirstName($response['firstName'])
+      ->setPwd1($response['pwd1'])
+      ->setTelephone($response['telephone'])
+      ->setGender($response['gender']);
+    if ($response['address']) {
+      $account->setAddress($this->decodeAddressResponse($response['address']));
     }
-    if ($response->business) {
-      $account->setBusiness($this->decodeBusinessResponse($response->business));
+    if ($response['business']) {
+      $account->setBusiness($this->decodeBusinessResponse($response['business']));
     }
     return $account;
   }
 
   public function decodeAddressResponse($response) {
     $address = new Address();
-    $address->setId($response->id)
-      ->setFirstName($response->firstName)
-      ->setFamilyName($response->familyName)
-      ->setAddressLine1($response->addressLine1)
-      ->setLocality($response->locality)
-      ->setPostalCode($response->postalCode)
-      ->setCountryCode($response->countryCode);
+    $address->setId($response['id'])
+      ->setFirstName($response['firstName'])
+      ->setFamilyName($response['familyName'])
+      ->setAddressLine1($response['addressLine1'])
+      ->setLocality($response['locality'])
+      ->setPostalCode($response['postalCode'])
+      ->setCountryCode($response['countryCode']);
     return $address;
   }
 
   public function decodeBusinessResponse($response) {
     $business = new Business();
-    $business->setId($response->id)
-      ->setName($response->name)
-      ->setTelephone($response->telephone)
-      ->setEanNumber($response->eanNumber)
-      ->setCvrNumber($response->cvrNumber)
-      ->setElectronicInvoice($response->electronicInvoice)
-      ->setMonthlyInvoice($response->monthlyInvoice)
-      ->setCompanyReference($response->companyReference)
-      ->setFax($response->fax);
-    if ($response->address) {
-      $business->setAddress($this->decodeAddressResponse($response->address));
+    $business->setId($response['id'])
+      ->setName($response['name'])
+      ->setTelephone($response['telephone'])
+      ->setEanNumber($response['eanNumber'])
+      ->setCvrNumber($response['cvrNumber'])
+      ->setElectronicInvoice($response['electronicInvoice'])
+      ->setMonthlyInvoice($response['monthlyInvoice'])
+      ->setCompanyReference($response['companyReference'])
+      ->setFax($response['fax']);
+    if ($response['address']) {
+      $business->setAddress($this->decodeAddressResponse($response['address']));
     }
     return $business;
   }
